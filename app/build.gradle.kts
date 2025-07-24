30c312aead27b3c62d65ae7e8467c8282080c14d
Adam Poterałowicz <adam.poteralowicz@gmail.com>
2025-07-24 08:53:50 +0200

    Add dependencies for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,11 @@ dependencies {
 
     implementation(libs.joda)
 
+    testImplementation(libs.androidx.arch.core.test)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
